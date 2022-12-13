ecfd0dd2a14c9da751bb51e59f2fbf0fef3842c6
fajarnurrahman09 <62900256+fajarnurrahman09@users.noreply.github.com>
2022-12-13 15:38:50 +0700

    update ke-4

--- resources/views/bgame.blade.php
+++ resources/views/bgame.blade.php
@@ -49,19 +49,19 @@
             <div class="form-check">
               <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
               <label class="form-check-label" for="flexRadioDefault1">
-                Mudah
+                Mudah (Kelas 7)
               </label>
             </div>
             <div class="form-check">
               <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
               <label class="form-check-label" for="flexRadioDefault2">
-                Sedang
+                Sedang (Kelas 8)
               </label>
             </div>
             <div class="form-check">
               <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
               <label class="form-check-label" for="flexRadioDefault1">
-                Sulit
+                Sulit (Kelas 9)
               </label>
             </div>
           </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <header>
     <nav class="navbar">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">BioFunQ</a>
+            <a class="navbar-brand" href="/home">BioFunQ</a>
             <a href="login"><button type="button" class="btn btn-primary">Login</button></a>
         </div>
 
